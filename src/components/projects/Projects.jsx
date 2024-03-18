4609f9bb62bdc6b28cfa0fe1782543bf2346576b
Arron.Reed <arron.reed@chasacademy.se>
2024-03-19 00:03:19 +0100

    Projects section styled

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -14,9 +14,9 @@ const Projects = () => {
       <div className="project-container">
         <div className="project">
           <div>
-            <img src="" alt="weather app" />
+            <img src="./project1.png" alt="weather app" />
           </div>
-          <div>
+          <div className="project-content">
             <div className="tag">
               <h5>Skifield App</h5>
             </div>
@@ -32,6 +32,28 @@ const Projects = () => {
             </div>
           </div>
         </div>
+
+        <div className="project">
+          <div className="project-content2">
+            <div className="tag2">
+              <h5>Skifield App</h5>
+            </div>
+            <div className="title">
+              <h3>FullStack Skifield Application</h3>
+            </div>
+            <div className="description">
+              <p>
+                A full-stack app with React, Express.js, Sass, TypeScript, and
+                MongoDB, offering information on Swedens ski fields, including
+                maps, skifield info and pricing
+              </p>
+            </div>
+          </div>
+          <div className="img2">
+            <img src="./project1.png" alt="weather app" />
+          </div>
+        </div>
+
       </div>
     </section>
   );
