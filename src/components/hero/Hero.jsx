0df036221d8531b3eeef667888d6e13b4f0fe1e8
Arron.Reed <arron.reed@chasacademy.se>
2024-04-29 00:18:41 +0200

    Changed portfolio design - restarted

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -4,7 +4,16 @@ const Hero = () => {
 
     return (
     <div className="hero">
-        <h1>Hello Hero</h1>
+        <div className="hero-left">
+            <div className="top-text">
+                <h3>Hello, I'm</h3>
+                <h1>Arron Reed</h1>
+                <h3>FullStack Developer & Web Designer</h3>
+            </div>
+        </div>
+        <div className="hero-image">
+            <img src="/hero-me.png" alt="Arron Reed" />
+        </div>
     </div>
     )
 }
