e369a5e252088b539aff57942e4df45c8f1df2e8
Arron.Reed <arron.reed@chasacademy.se>
2024-04-30 12:08:00 +0200

    hero section getting closer

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,21 +1,37 @@
-import "./Hero.scss"
+import "./Hero.scss";
 
 const Hero = () => {
-
-    return (
+  return (
     <div className="hero">
-        <div className="hero-left">
-            <div className="top-text">
-                <h3>Hello, I'm</h3>
-                <h1>Arron Reed</h1>
-                <h3>FullStack Developer & Web Designer</h3>
-            </div>
+      <div className="hero-left">
+        <div className="top-text">
+          <h3>Hello, I'm a</h3>
+          <h2>FullStack Developer</h2>
+          <p>
+            Hi, Im Arron Reed, a passionate FullStack developer and Web Designer
+            based in Stockholm. Welcome to my portfolio
+          </p>
         </div>
-        <div className="hero-image">
-            <img src="/hero-me.png" alt="Arron Reed" />
+        <div className="hero-icons">
+          <div className="icon-circle">
+            <i id="icons" className="fa-brands fa-facebook-f"></i>
+          </div>
+          <div className="icon-circle">
+            <i id="icons" className="fa-brands fa-github"></i>
+          </div>
+          <div className="icon-circle">
+            <i id="icons" className="fa-brands fa-instagram"></i>
+          </div>
+          <div className="icon-circle">
+            <i id="icons" className="fa-brands fa-linkedin-in"></i>
+          </div>
         </div>
+      </div>
+      <div className="hero-image">
+        <img src="/hero-me2.png" alt="Arron Reed" />
+      </div>
     </div>
-    )
-}
+  );
+};
 
-export default Hero
+export default Hero;
