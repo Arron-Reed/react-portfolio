1f09e34a5362290b9b8fe87293bc9a67bd6e2da1
Arron.Reed <arron.reed@chasacademy.se>
2024-07-29 00:47:38 +0200

    more updates.

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -25,42 +25,56 @@ const Hero = () => {
 
     const timeout = setTimeout(() => {
       setSubIndex((prevSubIndex) => prevSubIndex + (reverse ? -1 : 1));
-    }, Math.max(reverse ?50 : subIndex === jobTitles[index].length ? 1000 : 150, parseInt(Math.random() * 350)));
+    }, Math.max(reverse ? 50 : subIndex === jobTitles[index].length ? 1000 : 150, parseInt(Math.random() * 350)));
 
     return () => clearTimeout(timeout);
   }, [subIndex, index, reverse]);
 
   return (
-    <div className="hero">
-      <div className="hero-left">
-        <div className="top-text">
-          <h3>Hi, I'm</h3>
-          <h1>Arron Reed</h1>
+    <div className="wrapper">
+      <div className="hero">
+        <div className="hero-left">
+          <div className="top-text">
+            <h3>Hi, I'm</h3>
+            <h1>Arron Reed</h1>
+          </div>
+          <div className="top-text">
+            <h2>{`${jobTitles[index].substring(0, subIndex)}`}</h2>
+          </div>
+          <div className="hero-icons">
+            <div className="icon-circle">
+              <i id="icons" className="fa-brands fa-facebook-f"></i>
+            </div>
+            <div className="icon-circle">
+              <i id="icons" className="fa-brands fa-github"></i>
+            </div>
+            <div className="icon-circle">
+              <i id="icons" className="fa-brands fa-instagram"></i>
+            </div>
+            <div className="icon-circle">
+              <i id="icons" className="fa-brands fa-linkedin-in"></i>
+            </div>
+          </div>
         </div>
-        <div className="top-text">
-          <h2>{`${jobTitles[index].substring(0, subIndex)}`}</h2>
-          <p>
-            Hi, Im Arron Reed, a passionate FullStack developer and Web Designer
-            living in Stockholm. Welcome to my portfolio
-          </p>
-        </div>
-        <div className="hero-icons">
-          <div className="icon-circle">
-            <i id="icons" className="fa-brands fa-facebook-f"></i>
-          </div>
-          <div className="icon-circle">
-            <i id="icons" className="fa-brands fa-github"></i>
-          </div>
-          <div className="icon-circle">
-            <i id="icons" className="fa-brands fa-instagram"></i>
-          </div>
-          <div className="icon-circle">
-            <i id="icons" className="fa-brands fa-linkedin-in"></i>
-          </div>
+        <div className="hero-image">
+          <img src="/hero-me5.png" alt="Arron Reed" />
         </div>
       </div>
-      <div className="hero-image">
-        <img src="/hero-me5.png" alt="Arron Reed" />
+      <div className="infoBlock">
+        <div className="infoBlock-top">
+          <img src="ARlogo-column.png" alt="Arron Reed" />
+        </div>
+        <div className="infoBlock-text">
+          <p>
+            Hi, Im Arron Reed, a passionate FullStack developer and Web Designer
+            living in Stockholm. Welcome to my portfolio. Hi, Im Arron Reed, a
+            passionate FullStack developer and Web Designer living in Stockholm.
+            Welcome to my portfolio. Hi, Im Arron Reed, a passionate FullStack
+            developer and Web Designer living in Stockholm. Welcome to my
+            portfolio.
+          </p>
+        </div>
+        <button>See my CV</button>
       </div>
     </div>
   );
